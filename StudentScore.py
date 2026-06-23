2a3aa3009e4f9643d9667e00c194eb9573c59f91
pornima22 <pournima.vc22@gmail.com>
2026-06-23 15:18:34 +0530

    Initial commit

---
+++ StudentScore.py
@@ -0,0 +1,13 @@
+score = int(input("Enter your score: \n"));
+
+if score >= 90:
+    print("A")
+elif score >= 80:
+    print("B")
+elif score >= 70:
+    print("C")
+elif score >= 60:
+    print("D")
+elif score < 60:
+    print("F")
+
