0e4d99ca2ff78954c95768945656e15071a064a2
pornima22 <pournima.vc22@gmail.com>
2026-06-23 15:38:38 +0530

    Committed assignments

---
+++ Assignment-2/StudentScore.py
@@ -0,0 +1,14 @@
+score = int(input("Enter student score: \n"));
+
+if score >= 90:
+    print("A")
+elif score >= 80:
+    print("B")
+elif score >= 70:
+    print("C")
+elif score >= 60:
+    print("D")
+elif score < 60:
+    print("F")
+
+
