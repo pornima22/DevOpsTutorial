54d3f27d0bbef758557b49e820ede9db672a17ed
pornima22 <pournima.vc22@gmail.com>
2026-06-23 16:32:02 +0530

    committed assingment

---
+++ Assignment-2/WriteToFile.py
@@ -0,0 +1,8 @@
+file = open('data.txt', 'w')
+
+file.write("Hello, World!\n")
+file.write("This is a sample text file.\n")
+file.write("Created using Python.")
+
+print("Content written successfully.")
+file.close()
